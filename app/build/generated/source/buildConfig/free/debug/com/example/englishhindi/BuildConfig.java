7145bfdc46b7e75607d36e21fe8e0bd99517021f
Sunil Kumar Shah <babusunil49@gmail.com>
2025-06-07 18:14:19 +0530

    Copy files renamed

---
+++ app/build/generated/source/buildConfig/free/debug/com/example/englishhindi/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.englishhindi;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.englishhindi.free.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "free";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1.0-free-debug";
+  // Field from product flavor: free
+  public static final boolean PREMIUM_FEATURES_ENABLED = false;
+}
